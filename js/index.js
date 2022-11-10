559afbaa51f43ea4ecd5a9a8914873f17d00b68d
Rysth <johnpalacios.@gmail.com>
2022-11-10 11:22:51 -0500

    Update Nav Bar JS

--- js/index.js
+++ js/index.js
@@ -2,13 +2,23 @@ const navbarToggler = document.querySelector("#nav-toggler");
 const provitionalNavBar = document.querySelector(".provitional-navbar");
 const navbarLinks = document.querySelectorAll(".provitional-navbar ul li a");
 
-navbarToggler.addEventListener("click", () =>{
-  provitionalNavBar.classList.toggle("display-initial");
+window.onclick = function (event) {
+  console.log(event)
+  if (event.target !== navbarToggler) {
+    toggleNavList();
+  }
+};
+
+navbarToggler.addEventListener("click", () => {
+  toggleNavList();
 });
 
-navbarLinks.forEach(element => {
-  element.addEventListener("click", () =>{
-    provitionalNavBar.classList.toggle("display-initial");
+navbarLinks.forEach((element) => {
+  element.addEventListener("click", () => {
+    toggleNavList();
   });
 });
 
+function toggleNavList() {
+  provitionalNavBar.classList.toggle("display-initial");
+}
