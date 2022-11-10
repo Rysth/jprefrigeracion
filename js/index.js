050364ae740c0392af383c14249de7550c13fe99
Rysth <johnpalacios.@gmail.com>
2022-11-10 13:10:29 -0500

    Fixed JS Update

--- js/index.js
+++ js/index.js
@@ -2,19 +2,9 @@ const navbarToggler = document.querySelector("#nav-toggler");
 const provitionalNavBar = document.querySelector(".provitional-navbar");
 const navbarLinks = document.querySelectorAll(".provitional-navbar ul li a");
 
-navbarToggler.addEventListener("click", () => {
-  toggleNavList();
-});
-
-navbarLinks.forEach((element) => {
-  element.addEventListener("click", () => {
-    toggleNavList();
-  });
-});
-
 window.onclick = function (event) {
   navbarLinks.forEach((element) => {
-    if(event.target !== element && event.target !== navbarToggler){
+    if(event.target === element || event.target === navbarToggler){
         toggleNavList();
     }
   });
