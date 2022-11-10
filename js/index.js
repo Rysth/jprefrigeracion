686eeb352cb0a7e3f6fdd3f06a411de4b07e4302
Rysth <johnpalacios.@gmail.com>
2022-11-10 13:52:00 -0500

    Last JS Fix

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
 const navbarToggler = document.querySelector("#nav-toggler");
 const provitionalNavBar = document.querySelector(".provitional-navbar");
-const navbarLinks = document.querySelectorAll(".provitional-navbar ul li a");
+const navbarLinks = document.querySelectorAll(".provitional-navbar ul li");
 
-window.onclick = function (event) {
-  navbarLinks.forEach((element) => {
-    if(event.target === element || event.target === navbarToggler){
-        toggleNavList();
+window.onclick = (event) => {
+  for (let index = 0; index < navbarLinks.length; index++) {
+    const element = navbarLinks[index];
+    if (event.target !== navbarToggler && event.target !== element) {
+      removeNavList();
+      break;
     }
-  });
+  }
 };
 
-function toggleNavList() {
-  provitionalNavBar.classList.toggle("display-initial");
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    addNavList();
+  });
+});
+
+navbarToggler.addEventListener("click", () => {
+  addNavList();
+});
+
+function addNavList() {
+  provitionalNavBar.classList.add("display-initial");
+}
+
+function removeNavList() {
+  provitionalNavBar.classList.remove("display-initial");
 }
